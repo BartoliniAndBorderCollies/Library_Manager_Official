254f5e38e92123a70efa0d5f49bf797b1b0d970b
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-29 18:14:09 +0200

    Add test code to check connection with DB

---
+++ src/main/java/org/klodnicki/Main.java
@@ -0,0 +1,18 @@
+package org.klodnicki;
+
+import org.hibernate.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        // Check database version (because there is nothing else in database)
+        String sql = "select version()";
+
+        String result = session.createNativeQuery(sql, String.class).getSingleResult();
+        System.out.println(result);
+
+        session.close();
+        HibernateUtil.shutdown();
+    }
+}
