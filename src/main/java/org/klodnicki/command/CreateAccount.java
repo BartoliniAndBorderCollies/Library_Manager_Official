91c1ec0aef2720ff5eb2f1e8f76cb58428368967
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-30 11:26:37 +0200

    add execute() in command class CreateAccount

--- src/main/java/org/klodnicki/command/CreateAccount.java
+++ src/main/java/org/klodnicki/command/CreateAccount.java
@@ -1,4 +1,17 @@
 package org.klodnicki.command;
 
-public class CreateAccount {
+import org.klodnicki.controller.AccountController;
+
+public class CreateAccount implements MenuCommand{
+
+    private final AccountController accountController;
+
+    public CreateAccount(AccountController accountController) {
+        this.accountController = accountController;
+    }
+
+    @Override
+    public void execute() {
+        accountController.create();
+    }
 }
