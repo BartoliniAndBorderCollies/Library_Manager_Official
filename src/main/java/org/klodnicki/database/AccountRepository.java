8bfd0ff26d6110ea53ea41ab3f6e304e9ee70407
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-30 11:24:58 +0200

    add create() in AccountRe

--- src/main/java/org/klodnicki/database/AccountRepository.java
+++ src/main/java/org/klodnicki/database/AccountRepository.java
@@ -1,4 +1,17 @@
 package org.klodnicki.database;
 
+import jakarta.persistence.EntityManager;
+import org.hibernate.SessionFactory;
+import org.klodnicki.entities.Account;
+
 public class AccountRepository {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+    private final EntityManager entityManager = sessionFactory.createEntityManager();
+
+
+    public void create(Account account) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(account);
+        entityManager.getTransaction().commit();
+    }
 }
