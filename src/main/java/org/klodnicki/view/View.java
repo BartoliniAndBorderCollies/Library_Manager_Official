ce338c31b6ed42f46f0e39a3ab2a00d38215c64a
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-30 11:16:33 +0200

    add methods in View to print messages

--- src/main/java/org/klodnicki/view/View.java
+++ src/main/java/org/klodnicki/view/View.java
@@ -1,4 +1,28 @@
 package org.klodnicki.view;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class View {
+    private final List<String> lines = new ArrayList<>();
+
+    public void addLine(String text) {
+        lines.add(text);
+    }
+
+    public void clearLines() {
+        lines.clear();
+    }
+
+    public void print() {
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+
+    public void update(String text) {
+        addLine(text);
+        print();
+        clearLines();
+    }
 }
