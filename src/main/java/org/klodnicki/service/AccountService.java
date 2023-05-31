9d666da3493526163d13c450ab634b9a28166b89
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-31 13:55:47 +0200

    add logic for creating account regarding phone or email

--- src/main/java/org/klodnicki/service/AccountService.java
+++ src/main/java/org/klodnicki/service/AccountService.java
@@ -2,12 +2,17 @@ package org.klodnicki.service;
 
 import org.klodnicki.repository.AccountRepository;
 import org.klodnicki.entity.Account;
+import org.klodnicki.view.MenuView;
 
 public class AccountService {
     private final AccountRepository accountRepository = new AccountRepository();
 
+
     public void create(String firstName, String secondName, String lastName, int pesel, int phoneNumber, String email, String address) {
-        // TODO: check if there is given all required fields, and one of the optional phone or email.
+        if(phoneNumber == 0 && email == null) {
+            System.out.println("Phone number or email address must have a value."); //TODO: sout shouldnt be here
+            return;
+        }
         accountRepository.create(new Account(firstName, secondName, lastName, pesel, phoneNumber, email, address));
     }
 }
