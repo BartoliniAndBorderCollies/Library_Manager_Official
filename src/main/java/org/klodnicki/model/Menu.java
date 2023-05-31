2f58ca070236ac43ef2924e34f0fda163ef25100
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-05-31 12:54:42 +0200

    add executing command functionality

--- src/main/java/org/klodnicki/model/Menu.java
+++ src/main/java/org/klodnicki/model/Menu.java
@@ -1,14 +1,27 @@
 package org.klodnicki.model;
 
+import org.klodnicki.command.CreateAccount;
 import org.klodnicki.command.MenuCommand;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Menu {
-    private final List<MenuCommand> commands = new ArrayList<>();
+    private final List<MenuCommand> commands = new ArrayList<>();;
 
     public void addCommand(MenuCommand command) {
         commands.add(command);
+    }
+
+    public List<MenuCommand> getCommands() {
+        return commands;
+    }
+
+    public void executeCommand(String userInput) {
+        for (MenuCommand command: commands) {
+            if(userInput.equalsIgnoreCase(command.getName())) {
+                command.execute();
+            }
+        }
     }
 }
