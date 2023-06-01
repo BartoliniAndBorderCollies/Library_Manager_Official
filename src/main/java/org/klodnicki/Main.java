0b716a1aa18ef5c5a75c7575fd3a940a90c4383c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-01 14:55:03 +0200

    deletion of loop in Main

--- src/main/java/org/klodnicki/Main.java
+++ src/main/java/org/klodnicki/Main.java
@@ -5,9 +5,7 @@ import org.klodnicki.controller.MenuController;
 public class Main {
     public static void main(String[] args) {
 
-        do {
             MenuController menuController = new MenuController();
             menuController.run();
-        }while(true);
     }
 }
