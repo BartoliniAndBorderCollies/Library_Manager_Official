0a19aaef815df66309022d1dd86a0eeba86865ad
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-05 13:42:06 +0200

    add entity Book with necessary fields

---
+++ src/main/java/org/klodnicki/entity/Book.java
@@ -0,0 +1,42 @@
+package org.klodnicki.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+    @Column(nullable = false)
+    private String author;
+    @Column
+    private String isbn;
+    @Column
+    private String publisher;
+    @Column(name = "publication_year")
+    private int publicationYear;
+    @Column
+    private int edition;
+    @Column
+    private String genre;
+    @Column
+    private String description;
+    @Column
+    private String language;
+    @Column(name = "copies_number")
+    private int copiesNumber;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
+
