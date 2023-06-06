85ff20f753f9c49ccfa022fc2a2c7a913d9c90e5
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-06 15:26:26 +0200

    add command lend a book to menuCommand

--- src/main/java/org/klodnicki/service/MenuService.java
+++ src/main/java/org/klodnicki/service/MenuService.java
@@ -2,9 +2,11 @@ package org.klodnicki.service;
 
 import org.klodnicki.command.AddBook;
 import org.klodnicki.command.CreateAccount;
+import org.klodnicki.command.LendBook;
 import org.klodnicki.command.MenuCommand;
 import org.klodnicki.controller.AccountController;
 import org.klodnicki.controller.BookController;
+import org.klodnicki.controller.LendBookController;
 import org.klodnicki.controller.MenuController;
 import org.klodnicki.model.Menu;
 
@@ -17,6 +19,7 @@ public class MenuService {
     public void addMainMenuCommands(MenuController menuController) { // tu jest taki parametr bo AccountController go potrzebuje
         menu.addCommand(new CreateAccount(new AccountController(menuController)));
         menu.addCommand(new AddBook(new BookController(menuController)));
+        menu.addCommand(new LendBook(new LendBookController(menuController)));
     }
 
     public boolean executeCommand(String userInput) {
