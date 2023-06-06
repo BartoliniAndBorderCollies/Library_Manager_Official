3ad5a99001f7c04e2cb9e36f34537e4b9e1a3452
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-06 15:27:11 +0200

    add interaction with user to get demanded info to lend a book

---
+++ src/main/java/org/klodnicki/controller/LendBookController.java
@@ -0,0 +1,39 @@
+package org.klodnicki.controller;
+
+import org.klodnicki.service.LendBookService;
+
+public class LendBookController {
+
+    private final MenuController menuController;
+
+    private final LendBookService lendBookService = new LendBookService();
+
+    public LendBookController(MenuController menuController) {
+        this.menuController = menuController;
+    }
+
+    private static final String LEND_BOOK_PROCEDURE = "In order to lend a book please fill the following information. " +
+            "\nRemember that each reader can have maximum 10 books borrowed at the same time.";
+    private static final String READER_INFORMATION = "Reader infomation:";
+    private static final String FIRST_NAME = "First name:";
+    private static final String LAST_NAME = "Last name:";
+    private static final String PESEL = "Pesel:";
+    private static final String BOOK_INFORMATION = "Book information:";
+    private static final String BOOK_TITLE = "Title of a book:";
+    private static final String AUTHOR = "Author:";
+
+
+    public void lend() {
+        menuController.displayOnMenu(LEND_BOOK_PROCEDURE);
+        menuController.displayOnMenu(READER_INFORMATION);
+        String firstName = menuController.displayOnMenuAndAskForInput(FIRST_NAME);
+        String lastName = menuController.displayOnMenuAndAskForInput(LAST_NAME);
+        String pesel = menuController.displayOnMenuAndAskForInput(PESEL);
+
+        menuController.displayOnMenu(BOOK_INFORMATION);
+        String title = menuController.displayOnMenuAndAskForInput(BOOK_TITLE);
+        String author = menuController.displayOnMenuAndAskForInput(AUTHOR);
+
+        lendBookService.lend(firstName, lastName, pesel, title, author);
+    }
+}
