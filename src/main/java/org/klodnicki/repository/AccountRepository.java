3e63cc234c9a6af66615a76dde5f51dcddf846fc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-07 13:41:57 +0200

    add method to find an Account in the database by first and last names and PESEL

--- src/main/java/org/klodnicki/repository/AccountRepository.java
+++ src/main/java/org/klodnicki/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package org.klodnicki.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.klodnicki.entity.Account;
 
 public class AccountRepository {
@@ -10,5 +11,15 @@ public class AccountRepository {
         entityManager.getTransaction().begin();
         entityManager.persist(account);
         entityManager.getTransaction().commit();
+    }
+
+    public Account findAccountByFirstNameAndLastNameAndPesel(String firstName, String lastName, String pesel) {
+        String hqlQuery = "FROM Account a WHERE a.firstName = :firstName AND a.lastName = :lastName AND a.pesel = :pesel";
+        TypedQuery<Account> query = entityManager.createQuery(hqlQuery, Account.class);
+        query.setParameter("firstName", firstName);
+        query.setParameter("lastName", lastName);
+        query.setParameter("pesel", pesel);
+
+        return query.getSingleResult();
     }
 }
