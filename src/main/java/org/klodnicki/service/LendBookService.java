8240d6ffe4d9ca48afbd830224e4411dab87bbfc
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-07 22:32:45 +0200

    add logic in service layer to make possible to lend a book

--- src/main/java/org/klodnicki/service/LendBookService.java
+++ src/main/java/org/klodnicki/service/LendBookService.java
@@ -15,6 +15,19 @@ public class LendBookService {
 
     public void lend(String firstName, String lastName, String pesel, String title, String author) {
 
+        // nie mialem pola account a chcialem zastosowac metode addBook, pole nie moze byc wiec account uzyskalem
+        //poprzez ponizszy find
+
+        Account account = findAccountByFirstNameAndLastNameAndPesel(firstName, lastName, pesel);
+        Book book = findBookByTitleAndAuthor(title, author);
+
+        account.addBook(book);
+        bookService.update(book);
+
+        //wypożyczyć tzn przypisać daną książkę do danego konta, czyli dodać ją do listy?
+        // ilość kopii to ilość dostępnych egzemplarzy?
+        // przypisanie książki do konta zmniejszą o jeden ilość dostępnych kopii
+
     }
 
     public Account findAccountByFirstNameAndLastNameAndPesel(String firstName, String lastName, String pesel) {
