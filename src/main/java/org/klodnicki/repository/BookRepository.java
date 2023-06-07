152c8efb99d07fe7fc7cd634571ab4db232ee7a8
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-07 15:22:18 +0200

    add finding book by title and author using hibernate framework

--- src/main/java/org/klodnicki/repository/BookRepository.java
+++ src/main/java/org/klodnicki/repository/BookRepository.java
@@ -1,6 +1,7 @@
 package org.klodnicki.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
 import org.klodnicki.entity.Book;
 
 public class BookRepository {
@@ -13,4 +14,12 @@ public class BookRepository {
         entityManager.getTransaction().commit();
     }
 
+    public Book findBookByTitleAndAuthor(String title, String author) {
+        String hqlQuery = "FROM Book b WHERE b.title = :title AND b.author = :author";
+        TypedQuery<Book> query = entityManager.createQuery(hqlQuery, Book.class);
+        query.setParameter("title", title);
+        query.setParameter("author", author);
+
+        return query.getSingleResult();
+    }
 }
