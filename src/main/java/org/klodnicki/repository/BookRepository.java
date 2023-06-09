8fef9ef61918dd2937bed3a638663af5b590eefa
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-09 13:20:43 +0200

    renaming a class name in HQL

--- src/main/java/org/klodnicki/repository/BookRepository.java
+++ src/main/java/org/klodnicki/repository/BookRepository.java
@@ -2,30 +2,30 @@ package org.klodnicki.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.TypedQuery;
-import org.klodnicki.entity.Book;
+import org.klodnicki.entity.BookInfo;
 
 public class BookRepository {
 
     private final EntityManager entityManager = HibernateUtil.getSessionFactory().createEntityManager();
 
-    public void add(Book book) {
+    public void add(BookInfo bookInfo) {
         entityManager.getTransaction().begin();
-        entityManager.persist(book);
+        entityManager.persist(bookInfo);
         entityManager.getTransaction().commit();
     }
 
-    public Book findBookByTitleAndAuthor(String title, String author) {
-        String hqlQuery = "FROM Book b WHERE b.title = :title AND b.author = :author";
-        TypedQuery<Book> query = entityManager.createQuery(hqlQuery, Book.class);
+    public BookInfo findBookByTitleAndAuthor(String title, String author) {
+        String hqlQuery = "FROM BookInfo b WHERE b.title = :title AND b.author = :author";
+        TypedQuery<BookInfo> query = entityManager.createQuery(hqlQuery, BookInfo.class);
         query.setParameter("title", title);
         query.setParameter("author", author);
 
         return query.getSingleResult();
     }
 
-    public void update(Book book) {
+    public void update(BookInfo bookInfo) {
         entityManager.getTransaction().begin();
-        entityManager.merge(book);
+        entityManager.merge(bookInfo);
         entityManager.getTransaction().commit();
     }
 }
