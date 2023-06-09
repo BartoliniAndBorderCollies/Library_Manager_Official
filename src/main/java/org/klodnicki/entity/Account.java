438b4ba9baae836dcf9f9460a5a9e88afd8ea9e7
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-09 13:20:18 +0200

    add annotations to join columns from 2 tables

--- src/main/java/org/klodnicki/entity/Account.java
+++ src/main/java/org/klodnicki/entity/Account.java
@@ -2,6 +2,7 @@ package org.klodnicki.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
@@ -11,20 +12,18 @@ public class Account {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
-
-    @OneToMany (fetch = FetchType.LAZY, //jedno konto może miec wiele ksiązek
-    mappedBy = "account")
-    private List<Book> books;
-
+    @ManyToMany(fetch = FetchType.LAZY, //jak robię manyToMany to po obu stronach tej czynnosci musi byc adnotacja manyToMany
+            mappedBy = "accounts")
+    private List<BookInfo> bookInfos = new ArrayList<>(); //lista informacji o ksiązkach, które to konta ma wypożyczone
     @Column(nullable = false, name = "first_name")
     private String firstName;
-    @Column (name = "second_name")
+    @Column(name = "second_name")
     private String secondName;
     @Column(nullable = false, name = "last_name")
     private String lastName;
     @Column(nullable = false)
     private String pesel;
-    @Column (name = "phone_number")
+    @Column(name = "phone_number")
     private String phoneNumber;
     @Column
     private String email;
@@ -44,7 +43,7 @@ public class Account {
         this.address = address;
     }
 
-    public void addBook(Book book) {
-        books.add(book);
+    public List<BookInfo> getBooks() {
+        return bookInfos;
     }
 }
