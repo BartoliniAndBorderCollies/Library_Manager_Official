7700529d238785ff2dac7eb75c7e344f0c1a44a3
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-12 22:21:40 +0200

    add logic for book with more than 1 edition

--- src/main/java/org/klodnicki/service/BookService.java
+++ src/main/java/org/klodnicki/service/BookService.java
@@ -3,11 +3,13 @@ package org.klodnicki.service;
 import org.klodnicki.entity.BookInfo;
 import org.klodnicki.repository.BookRepository;
 
+import java.util.List;
+
 public class BookService {
 
     private final BookRepository bookRepository = new BookRepository();
 
-    public void add(String title, String author, String isbn, String publisher, int publicationYear, int edition,
+    public void add(String title, String author, String isbn, String publisher, int publicationYear, String edition,
                     String genre, String description, String language, int copiesNumber) {
 
         if(title.equals("") || author.equals("")) {
@@ -22,7 +24,15 @@ public class BookService {
         return bookRepository.findBookByTitleAndAuthor(title, author);
     }
 
+    public BookInfo findBookByTitleAndAuthorAndEdition(String title, String author, String edition) {
+        return bookRepository.findBookByTitleAndAuthorAndEdition(title, author, edition);
+    }
+
     public void update(BookInfo bookInfo) {
         bookRepository.update(bookInfo);
+    }
+
+    public List<BookInfo> findBooksByTitleAndAuthorReturnList (String title, String author) {
+        return bookRepository.findBooksByTitleAndAuthorReturnList(title, author);
     }
 }
