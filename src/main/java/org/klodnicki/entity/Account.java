33a27c3cdfbcd53dc985011737e70a69ebcd740e
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-19 19:25:32 +0200

    add equals to compare two objects and remove a book from list

--- src/main/java/org/klodnicki/entity/Account.java
+++ src/main/java/org/klodnicki/entity/Account.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "accounts")
@@ -49,5 +50,18 @@ public class Account {
 
     public Long getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return pesel.equals(account.pesel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pesel);
     }
 }
