e99943047f75b209108fc24297a80525c8b49909
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-24 21:00:47 +0200

    add new command "show accounts" to all commands menu

--- src/main/java/org/klodnicki/service/MenuService.java
+++ src/main/java/org/klodnicki/service/MenuService.java
@@ -15,12 +15,13 @@ public class MenuService {
         menu.addCommand(new AddBook(new BookController(menuController)));
         menu.addCommand(new LendBook(new LendBookController(menuController)));
         menu.addCommand(new ReturnBook(new ReturnBookController(menuController)));
+        menu.addCommand(new ShowAccounts(new AccountController(menuController)));
     }
 
     public boolean executeCommand(String userInput) {
         List<MenuCommand> commands = menu.getCommands();
-        for (MenuCommand command: commands) {
-            if(userInput.equalsIgnoreCase(command.getName())) {
+        for (MenuCommand command : commands) {
+            if (userInput.equalsIgnoreCase(command.getName())) {
                 menu.executeCommand(command);
                 return true;
             }
