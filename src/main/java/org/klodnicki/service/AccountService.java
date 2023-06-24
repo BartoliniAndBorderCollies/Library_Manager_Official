1da3bc452d08f701dbefd6593e7e2364a2a265ff
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-24 21:01:46 +0200

    add logic to provide all accounts to user

--- src/main/java/org/klodnicki/service/AccountService.java
+++ src/main/java/org/klodnicki/service/AccountService.java
@@ -1,7 +1,11 @@
 package org.klodnicki.service;
 
+import org.klodnicki.exception.NotFoundInDatabaseException;
 import org.klodnicki.repository.AccountRepository;
 import org.klodnicki.entity.Account;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountService {
     private final AccountRepository accountRepository = new AccountRepository();
@@ -9,18 +13,36 @@ public class AccountService {
     public void create(String firstName, String secondName, String lastName, String pesel, String phoneNumber,
                        String email, String address) {
 
-            if (pesel.length() != 11) {
-                throw new IllegalArgumentException("Pesel must have 11 digits.");
+        if (pesel.length() != 11) {
+            throw new IllegalArgumentException("Pesel must have 11 digits.");
 
-            }
-            if (phoneNumber.equals("") && email.equals("")){
-                throw new IllegalArgumentException("Phone number or email address must have a value.");
-            }
+        }
+        if (phoneNumber.equals("") && email.equals("")) {
+            throw new IllegalArgumentException("Phone number or email address must have a value.");
+        }
 
         accountRepository.create(new Account(firstName, secondName, lastName, pesel, phoneNumber, email, address));
     }
 
     public Account findAccountByFirstNameAndLastNameAndPesel(String firstName, String lastName, String pesel) {
         return accountRepository.findAccountByFirstNameAndLastNameAndPesel(firstName, lastName, pesel);
+    }
+
+    public List<String> prepareListOfAllAccounts() throws NotFoundInDatabaseException {
+        List<Account> allAccountsFromDatabase = getAllAccountsFromDatabase();
+        List<String> results = new ArrayList<>();
+
+        if (allAccountsFromDatabase.isEmpty()) {
+            throw new NotFoundInDatabaseException(Account.class);
+        }
+
+        for (int i = 0; i < allAccountsFromDatabase.size(); i++) {
+            results.add(allAccountsFromDatabase.get(i).toString());
+        }
+        return results;
+    }
+
+    private List<Account> getAllAccountsFromDatabase() {
+        return accountRepository.findAllAccounts();
     }
 }
