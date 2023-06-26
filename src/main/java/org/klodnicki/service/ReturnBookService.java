4e216d8b1ac95c3d1e9e875508b7f09d35af76ff
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-26 10:53:55 +0200

    add logic to count the amount of fine to be paid

--- src/main/java/org/klodnicki/service/ReturnBookService.java
+++ src/main/java/org/klodnicki/service/ReturnBookService.java
@@ -4,6 +4,7 @@ import org.klodnicki.entity.Account;
 import org.klodnicki.entity.BookInfo;
 import org.klodnicki.exception.NotFoundInDatabaseException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReturnBookService {
@@ -12,12 +13,15 @@ public class ReturnBookService {
 
     private final BookService bookService;
 
+    private static final int MAX_DAYS_LEND = 100;
+    private static final int FINE_PER_DAY = 2;
+
     public ReturnBookService(AccountService accountService, BookService bookService) {
         this.accountService = accountService;
         this.bookService = bookService;
     }
 
-    public List<String> prepareListOfBorrowedBooksByAccount(String  firstName, String lastName, String pesel) {
+    public List<String> prepareListOfBorrowedBooksByAccount(String firstName, String lastName, String pesel) {
         Account account = accountService.findAccountByFirstNameAndLastNameAndPesel(firstName, lastName, pesel);
 
         return bookService.prepareListOfBorrowedBooksByAccount(account);
@@ -28,14 +32,28 @@ public class ReturnBookService {
         Account account = accountService.findAccountByFirstNameAndLastNameAndPesel(firstName, lastName, pesel);
         BookInfo bookInfo = bookService.findBookByTitleAndAuthorAndEdition(title, author, edition);
 
+        if (countFineForKeepingBook(bookInfo) > 0) {
+            System.out.println("Fine to be paid:" + countFineForKeepingBook(bookInfo) + " $");
+        }
+        bookInfo.setReturningDate(LocalDateTime.now());
         bookInfo.removeAccount(account);
+
         int bookCopiesUpdate = bookInfo.getCopiesNumber() + 1;
         bookInfo.setCopiesNumber(bookCopiesUpdate);
+
 
         bookService.update(bookInfo);
     }
 
+    private int countFineForKeepingBook(BookInfo bookInfo) {
+        int lendingDate = bookInfo.getLendingDate().getDayOfYear();
+        int returningDate = LocalDateTime.now().getDayOfYear();
+        int totalDays = returningDate - lendingDate;
+        int fine = 0;
 
-
-
+        if (totalDays > MAX_DAYS_LEND) {
+            fine = (totalDays - MAX_DAYS_LEND) * FINE_PER_DAY;
+        }
+        return fine;
+    }
 }
