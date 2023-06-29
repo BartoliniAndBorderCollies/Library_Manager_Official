a2317d91d7943806c473a0c0c469ef7e9fe99e8c
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-06-29 22:32:36 +0200

    improve enum sort of account to solve camel case problem of fields

--- src/main/java/org/klodnicki/service/SortOptionAccount.java
+++ src/main/java/org/klodnicki/service/SortOptionAccount.java
@@ -2,21 +2,27 @@ package org.klodnicki.service;
 
 public enum SortOptionAccount {
 
-    FIRST_NAME("first name"),
-    SECOND_NAME("second name"),
-    LAST_NAME("last name"),
-    PESEL("pesel"),
-    PHONE_NUMBER("phone number"),
-    EMAIL("email"),
-    ADDRESS("address");
+    FIRST_NAME("first name", "firstName"),
+    SECOND_NAME("second name", "secondName"),
+    LAST_NAME("last name", "lastName"),
+    PESEL("pesel", "pesel"),
+    PHONE_NUMBER("phone number", "phoneNumber"),
+    EMAIL("email", "email"),
+    ADDRESS("address", "address");
 
     private final String sortName;
+    private final String hqlParameter;
 
-    SortOptionAccount(String sortName) {
+    SortOptionAccount(String sortName, String hqlParameter) {
         this.sortName = sortName;
+        this.hqlParameter = hqlParameter;
     }
 
     public String getSortName() {
         return sortName;
+    }
+
+    public String getHqlParameter() {
+        return hqlParameter;
     }
 }
