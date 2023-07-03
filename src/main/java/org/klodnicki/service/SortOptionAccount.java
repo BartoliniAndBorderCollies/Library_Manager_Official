549d4571717960593ea8bfc4556c383913541282
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-07-03 08:48:52 +0200

    refactor of name

--- src/main/java/org/klodnicki/service/SortOptionAccount.java
+++ src/main/java/org/klodnicki/service/SortOptionAccount.java
@@ -2,13 +2,13 @@ package org.klodnicki.service;
 
 public enum SortOptionAccount {
 
-    FIRST_NAME("first name: ", "firstName"),
-    SECOND_NAME("second name: ", "secondName"),
-    LAST_NAME("last name: ", "lastName"),
-    PESEL("pesel: ", "pesel"),
-    PHONE_NUMBER("phone number: ", "phoneNumber"),
-    EMAIL("email: ", "email"),
-    ADDRESS("address: ", "address");
+    FIRST_NAME("first name", "firstName"),
+    SECOND_NAME("second name", "secondName"),
+    LAST_NAME("last name", "lastName"),
+    PESEL("pesel", "pesel"),
+    PHONE_NUMBER("phone number", "phoneNumber"),
+    EMAIL("email", "email"),
+    ADDRESS("address", "address");
 
     private final String sortName;
     private final String hqlParameter;
--- src/main/java/org/klodnicki/service/SortOptionBookInfo.java
+++ src/main/java/org/klodnicki/service/SortOptionBookInfo.java
@@ -1,16 +1,16 @@
 package org.klodnicki.service;
 
 public enum SortOptionBookInfo {
-    TITLE("title: "),
-    AUTHOR("author: "),
-    ISBN("isbn: "),
-    PUBLISHER("publisher: "),
-    PUBLICATION_YEAR("publication year: "),
-    EDITION("edition: "),
-    GENRE("genre: "),
-    DESCRIPTION("description: "),
-    LANGUAGE("language: "),
-    COPIES_NUMBER("copies number: ");
+    TITLE("title"),
+    AUTHOR("author"),
+    ISBN("isbn"),
+    PUBLISHER("publisher"),
+    PUBLICATION_YEAR("publication year"),
+    EDITION("edition"),
+    GENRE("genre"),
+    DESCRIPTION("description"),
+    LANGUAGE("language"),
+    COPIES_NUMBER("copies number");
 
     private final String sortName;
 
