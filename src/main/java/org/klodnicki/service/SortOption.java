7992fe8e03daa0aa6e9c90cf3b3720f160ec8748
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-07-04 09:12:45 +0200

    add hql parameters to handle two words fields

--- src/main/java/org/klodnicki/service/SortOption.java
+++ src/main/java/org/klodnicki/service/SortOption.java
@@ -1,24 +1,31 @@
 package org.klodnicki.service;
 
 public enum SortOption {
-    TITLE("title"),
-    AUTHOR("author"),
-    ISBN("isbn"),
-    PUBLISHER("publisher"),
-    PUBLICATION_YEAR("publication year"),
-    EDITION("edition"),
-    GENRE("genre"),
-    DESCRIPTION("description"),
-    LANGUAGE("language"),
-    COPIES_NUMBER("copies number");
+    TITLE("title", "title"),
+    AUTHOR("author", "author"),
+    ISBN("isbn", "isbn"),
+    PUBLISHER("publisher", "publisher"),
+    PUBLICATION_YEAR("publication year", "publicationYear"),
+    EDITION("edition", "edition"),
+    GENRE("genre", "genre"),
+    DESCRIPTION("description", "description"),
+    LANGUAGE("language", "language"),
+    COPIES_NUMBER("copies number", "copiesNumber");
 
     private final String sortName;
+    private final String hqlParameter;
 
-    SortOption(String sortName) {
+    SortOption(String sortName, String hqlParameter) {
         this.sortName = sortName;
+        this.hqlParameter = hqlParameter;
     }
+
 
     public String getSortName() {
         return sortName;
+    }
+
+    public String getHqlParameter() {
+        return hqlParameter;
     }
 }
