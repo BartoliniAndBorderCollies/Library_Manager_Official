1a5aa2d8523388624b4f9823656e710bcc9f1973
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-07-09 21:02:20 +0200

    refactor of bookInfo into lending ifo

--- src/main/java/org/klodnicki/service/AccountService.java
+++ src/main/java/org/klodnicki/service/AccountService.java
@@ -1,6 +1,7 @@
 package org.klodnicki.service;
 
 import org.klodnicki.entity.BookInfo;
+import org.klodnicki.entity.LendingInformation;
 import org.klodnicki.exception.NotFoundInDatabaseException;
 import org.klodnicki.exception.SortParameterNotFoundException;
 import org.klodnicki.repository.AccountRepository;
@@ -129,9 +130,9 @@ public class AccountService {
 
         List<String> results = new ArrayList<>();
         Account account = findAccountByFirstNameAndLastNameAndPesel(firstName, lastName, peselAccount);
-        List<BookInfo> books = account.getBooks();
+        List<LendingInformation> listOfBorrowedBooks = account.getLendingInformationAboutAccountList();
 
-        if (books.isEmpty()) {
+        if (listOfBorrowedBooks.isEmpty()) {
             throw new NotFoundInDatabaseException(BookInfo.class);
         }
 
@@ -140,7 +141,7 @@ public class AccountService {
         results.add(SortOptionAccount.PESEL.getSortName() + ": " + peselAccount);
         results.add("-----------------------------------------------------------");
 
-        for (LendingInformation book : books) {
+        for (LendingInformation book : listOfBorrowedBooks) {
             String title = book.getBookInfo().getTitle();
             String author = book.getBookInfo().getAuthor();
             String edition = book.getBookInfo().getEdition();
