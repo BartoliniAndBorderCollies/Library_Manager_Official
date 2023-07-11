c036c5de2715d42f272da553d131cff911359027
Bartolini <BARTEK.KLODNICKI@GMAIL.COM>
2023-07-11 11:55:00 +0200

    add remove method of lendingInformation in db

--- src/main/java/org/klodnicki/repository/LendingInformationRepository.java
+++ src/main/java/org/klodnicki/repository/LendingInformationRepository.java
@@ -1,7 +1,14 @@
 package org.klodnicki.repository;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.Query;
+import jakarta.persistence.TypedQuery;
+import org.klodnicki.entity.Account;
+import org.klodnicki.entity.BookInfo;
 import org.klodnicki.entity.LendingInformation;
+
+import java.util.Optional;
 
 public class LendingInformationRepository {
 
@@ -12,4 +19,16 @@ public class LendingInformationRepository {
         entityManager.persist(lendingInformation);
         entityManager.getTransaction().commit();
     }
+
+    public void remove(LendingInformation lendingInformation) {
+
+        entityManager.getTransaction().begin();
+        String hqlQuery = "DELETE FROM LendingInformation l WHERE l.id = :id";
+        Query query = entityManager.createQuery(hqlQuery);
+        query.setParameter("id", lendingInformation.getId());
+        query.executeUpdate();
+        entityManager.getTransaction().commit();
+    }
+
+
 }
